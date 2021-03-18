a0baf08f27516cd8fcfe29644070152cf4641d7a
Jack200133 <car20593@uvg.edu.gt>
2021-03-17 20:08:37 -0600

    Proyecto Terminado

---
+++ Proyecto Juegos/Library/Collab/Base/Assets/First person controller/FirstPersonMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+    Vector2 velocity;
+    public GameObject PauseMenu;
+    
+
+    void FixedUpdate()
+    {
+        if (PauseMenu.activeSelf == false)
+        {
+            velocity.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            transform.Translate(velocity.x, 0, velocity.y);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Nivel2"))
+        {            
+            SceneManager.LoadScene(2);
+        }
+            
+        if (other.gameObject.CompareTag("Nivel3"))
+        {
+            SceneManager.LoadScene(3);
+        }
+            
+
+
+    }
+}
